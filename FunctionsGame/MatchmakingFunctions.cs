f05b44559988bfb1a270ba2c4d817cad85717f19
Alex Kalkatos <alexcsts@gmail.com>
2022-12-28 01:00:21 -0300

    Matchmaking functions & subs update

---
+++ FunctionsGame/MatchmakingFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Kalkatos.FunctionsGame
+{
+	public static class MatchmakingFunctions
+	{
+		[FunctionName(nameof(FindMatch))]
+		public static async Task<IActionResult> FindMatch (
+			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] string identifier,
+			ILogger log)
+		{
+			await Task.Delay(100);
+			return new OkObjectResult("Ok");
+		}
+	}
+}
